6d1b6e0f48564869bddd1851cb857884c0eb17b2
Anrakutei VietNam <macmini2@laughing.local>
2025-12-17 14:01:12 +0700

    feat: authen page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { ROUTE_PATH } from '@/constants'
-import LoginPage from '@/pages/login-example'
 import NotFoundPage from '@/pages/not-found'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { MainLayout } from './components/layouts'
 import AboutPage from './pages/about'
 import HomePage from './pages/homepage'
+import LoginPage from './pages/login'
+import SignUpPage from './pages/sign-up'
 
 function App() {
 	return (
@@ -16,6 +17,10 @@ function App() {
 					<Route
 						path={ROUTE_PATH.AUTH.LOGIN}
 						element={<LoginPage />}
+					/>
+					<Route
+						path={ROUTE_PATH.AUTH.SIGNUP}
+						element={<SignUpPage />}
 					/>
 				</Route>
 				<Route path='*' element={<NotFoundPage />} />
